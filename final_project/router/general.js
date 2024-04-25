7f497e361c3339c58bbcb56f6ee6273431453fd8
andrewmcs <136806756+andrewmcs@users.noreply.github.com>
2024-04-25 16:50:25 -0400

    implemented task 3, get by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,15 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const author = req.params.author;
+    let books_found = {};
+
+    for (let isbn in books) {
+        if (books[isbn].author==author){
+            books_found[isbn]=books[isbn];
+        }
+    }
+    res.send(books_found);
 });
 
 // Get all books based on title
